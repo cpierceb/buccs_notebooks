ff1d199a46acf84b00e078bbb148448d373ca93c
cpierceb <charles.pierce@unibe.ch>
2026-08-10 15:46:35 +0200

    xgboost training backbone is complete

--- scripts/splitting_and_training.py
+++ scripts/splitting_and_training.py
@@ -1,12 +1,15 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
+import xgboost as xgb
 
 
 
 def split_data(X, y, mode = "random", quadrant = "ne"):
+    
     if mode == "random":
         X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, random_state=42)
+
         return X_train, X_val, y_train, y_val
 
     elif mode == "random_spatial":
@@ -54,3 +57,38 @@ def split_data(X, y, mode = "random", quadrant = "ne"):
         X_val = X_val.drop(columns=["station_id"]).copy()
 
         return X_train, X_val, y_train, y_val
+
+def train_model(X_train, X_val, y_train, y_val, model_type):
+    if model_type == "xgboost":
+        # params = {
+
+        #     "eta": 0.3,
+        #     "max_depth": 10,
+        #     "colsample_bytree": 0.8,
+        #     "subsample": 0.8,
+        #     "gamma": 1.0,
+        #     "min_child_weight": 1.0,
+        #     "reg_lambda": 1.0,
+        #     "reg_alpha": 0.0
+        # }
+
+        features = list(X_train.columns)
+
+        dtrain = xgb.DMatrix(X_train, label=y_train, feature_names=features)
+
+        dval = xgb.DMatrix(X_val, label=y_val, feature_names=features)
+
+        watchlist = [(dtrain, "train"), (dval, "valid")]
+
+        # params["objective"] = "reg:squarederror"
+        # params["eval_metric"] = "rmse" 
+
+        params = {"objective": "reg:squarederror", "eval_metric": "rmse"}
+
+        model = xgb.train(params, dtrain, num_boost_round=1000, evals=watchlist, 
+                            early_stopping_rounds=10, verbose_eval=15)
+
+
+        y_pred = model.predict(dval)
+
+        return y_pred
